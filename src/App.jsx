e1f2d8d4587674ef455e269cf977de13eebdb79d
GerardoZuno <gerardozunounitec@gmail.com>
2021-07-28 16:49:45 -0500

    Add Form and shortid package

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,31 @@
+import React, {useState} from 'react'
+import shortid from 'shortid';
+
+
 function App() {
+  const [tarea, setTarea] = useState('correr')
+  const [lista, setLista] = useState([])
+
+
+
+  const addTaks = (e) => {
+     e.preventDefault()
+
+     if(!tarea.trim()) {
+       console.log('esta vacio')
+       return
+     }
+     console.log(tarea)
+     setLista([
+       ...lista, {
+         id: shortid.generate(),
+         Description: tarea
+       }
+     ])
+
+     setTarea('')
+  }
+
   return (
     <div className="container mt-5">
       <h1 className="text-center">Hello World</h1>
@@ -17,6 +44,18 @@ function App() {
         </div>
         <div className="col-4">
           <h4 className="text-center">Formulario</h4>
+          <form onSubmit={addTaks}>
+            <input  
+            className='form-control mb-2'
+            type='text'
+            placeholder='ingrese tarea'
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+            />
+            <button className="btn btn-dark btn-block" type="submit">Agregar Tarea</button>
+            
+            
+          </form>
         </div>
       </div>
     </div>
