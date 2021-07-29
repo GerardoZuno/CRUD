207dfe705e32bbfde13a4e9e431e62c5302a2052
GerardoZuno <gerardozunounitec@gmail.com>
2021-07-29 14:25:35 -0500

    Completed Delete and Edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,66 @@
-import React, {useState} from 'react'
-import shortid from 'shortid';
-
+import React, { useState } from "react";
+import shortid from "shortid";
 
 function App() {
-  const [tarea, setTarea] = useState('correr')
-  const [lista, setLista] = useState([])
+  const [tarea, setTarea] = useState("caminar");
+  const [lista, setLista] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [idEdit, setEditId] = useState('');
+
+  
+
 
 
 
   const addTaks = (e) => {
-     e.preventDefault()
+    e.preventDefault();
 
-     if(!tarea.trim()) {
-       console.log('esta vacio')
-       return
-     }
-     console.log(tarea)
-     setLista([
-       ...lista, {
-         id: shortid.generate(),
-         Description: tarea
-       }
-     ])
+    if (!tarea.trim()) {
+      console.log("esta vacio");
+      return;
+    }
+    console.log(tarea);
+    setLista([
+      ...lista,
+      {
+        id: shortid.generate(),
+        description: tarea,
+      },
+    ]);
 
-     setTarea('')
+    setTarea("");
+  };
+
+  const deleteTask =  (id) => {
+    console.log(id);
+
+    const arrayFiltrado = lista.filter(item => item.id !== id); 
+    setLista(arrayFiltrado)
+  };
+
+  const updateTask = tarea => {
+    console.log(tarea);
+    setEdit(true)
+    setTarea(tarea.description)
+    setEditId(tarea.id)
+  }
+
+  const updatedTask = (e) => {
+    e.preventDefault();
+    console.log(tarea)
+    if (!tarea.trim()) {
+      console.log("esta vacio");
+      return;
+    }
+    const arrayEditado = lista.map
+    (item => item.id === idEdit ? {idEdit:idEdit, description:tarea} : item
+      )
+    setLista(arrayEditado)
+    setEdit(false)
+    setTarea('')
+    setEditId('')
+  
+
   }
 
   return (
@@ -34,27 +71,53 @@ function App() {
         <div className="col-8">
           <h4 className="text-center">Lista de Tareas</h4>
           <ul className="list-group">
-            <li className="list-group-item">
-              <span className="lead">Nombre de la Tarea</span>
-              <button className="btn btn-danger btn-sm float-right mx-2" >Eliminar</button>
-              <button className="btn btn-warning btn-sm">Editar</button>
-
-            </li>
+            {lista.map((tarea) => (
+              <li key={tarea.id} className="list-group-item">
+                <span className="lead">
+                  <strong>{tarea.description}</strong>
+                </span>
+                <button
+                  className="btn btn-danger btn-sm float-right mx-2"
+                  onClick={(e) => deleteTask(tarea.id)}
+                >
+                  Eliminar
+                </button>
+                <button 
+                className="btn btn-warning btn-sm"
+                onClick={(e) => updateTask(tarea)}
+                >
+                Editar
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="col-4">
-          <h4 className="text-center">Formulario</h4>
-          <form onSubmit={addTaks}>
-            <input  
-            className='form-control mb-2'
-            type='text'
-            placeholder='ingrese tarea'
-            onChange={(e) => setTarea(e.target.value)}
-            value={tarea}
+          <h4 className="text-center">
+            {
+              edit ? 'Editar Tarea' : 'Agregar Tarea'
+            }
+          </h4>
+          <form onSubmit={edit ? updatedTask : addTaks}>
+            <input
+              className="form-control mb-2"
+              type="text"
+              placeholder="ingrese tarea"
+              onChange={(e) => setTarea(e.target.value)}
+              value={tarea}
             />
-            <button className="btn btn-dark btn-block" type="submit">Agregar Tarea</button>
-            
-            
+            {
+              edit ? (
+                <button className="btn btn-warning btn-block" type="submit">
+                Editar
+              </button>
+              ) : (
+                <button className="btn btn-warning btn-block" type="submit">
+                Agregar
+              </button>
+              )
+            }
+           
           </form>
         </div>
       </div>
